37b2a2411d56c3c9ded181542d897e4ab8e7e6c2
kevin <chantaldzl@sina.com>
2018-06-17 21:30:15 +0800

    first commit study_note043 txt file

--- js/h5c3/study_code042.js
+++ js/h5c3/study_code042.js
@@ -9,6 +9,28 @@ $(function () {
             afterLoad: function (anchorLink, index) {
                 // 页面滚动到某一屏是的回调
                 $('.section:eq(' + (index - 1) + ')').addClass('now');
+                if (index == 7) {
+                    $('.section07 .star img').each(function (index) {
+                        $(this).delay(index * 0.5 * 1000).css('opacity', '1');
+                    });
+                }
+                if (index == 8) {
+                    $('.section08 .btn').on('mouseenter', function () {
+                        $('.section08 .btn img:first-child').css('opacity', '0');
+                    });
+                    $('.section08 .btn').on('mouseleave', function () {
+                        $('.section08 .btn img:first-child').css('opacity', '1');
+                    });
+                    $('.section08').on('mousemove', function (e) {
+                        console.log(e);
+                        $('.section08 .hand img').css({left: e.clientX - 140, top: e.clientY + 60});
+                    });
+                    $('.section08 .again').on('click', function () {
+                        $('.now , .leave').removeClass('.now').removeClass('.leave');
+                        $('.content [style]').removeAttr('style');
+                        $.fn.fullpage.moveTo(1);
+                    });
+                }
             },
             afterRender: function () {
                 $('.more').on('click', function () {
@@ -17,10 +39,9 @@ $(function () {
                 });
             },
             onLeave: function (index, nextIndex, direction) {
-                if (index == 2 && nextIndex == 3) {
-                    $('.section:eq(' + (index - 1) + ')').addClass('leave');
-                }
-                if (index == 3 && nextIndex == 4) {
+                if (index == 2 && nextIndex == 3 ||
+                    (index == 3 && nextIndex == 4) ||
+                    (index == 5 && nextIndex == 6)) {
                     $('.section:eq(' + (index - 1) + ')').addClass('leave');
                 }
             }
